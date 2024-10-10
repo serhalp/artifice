7a17a60673f44f6b77ecac061e65815cd98b8816
Philippe Serhal <philippe.serhal@gmail.com>
2024-10-09 20:48:01 -0400

    fix: use nitro nightly, switch back to Functions
    The `openai` package doesn't seem to work in the Netlify Edge runtime...
    but `@netlify/blobs` wasn't working out of the box on Netlify Functions,
    because Nitro stable (2.x) still uses Netlify "Lambda-compatible"
    Functions (sometimes called "Functions v1").
    
    Nitro nightly (upcoming v3) uses regular Netlify Functions, which should
    finally just work.

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
-    preset: "netlify-edge",
+    // @ts-expect-error(serhalp) -- This is because I'm opting into nitropack nightly which adds
+    // this, but SolidStart doesn't know I'm doing this.
+    compatibilityDate: "2024-09-24",
   },
 });
