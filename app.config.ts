aa2010b2e1042f80170201ea00b825d8c9a48b84
Philippe Serhal <philippe.serhal@gmail.com>
2024-11-24 10:08:23 -0500

    chore(deps): upgrade all deps again

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
-    // @ts-expect-error(serhalp) -- This is because I'm opting into nitropack nightly which adds
-    // this, but SolidStart doesn't know I'm doing this.
-    compatibilityDate: "2024-09-24",
+    compatibilityDate: "2024-11-24",
   },
 });
